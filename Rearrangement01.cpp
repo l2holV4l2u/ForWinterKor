d71785ee72a677ae2594098bb0452ec25f5ee58d
Naruesorn Prabpon <59469930+l2holV4l2u@users.noreply.github.com>
2023-03-21 07:19:46 +0700

    Create Rearrangement01.cpp

---
+++ Rearrangement01.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(0),cin.tie(0);
+    long long num,a[810],b[810],sum=0;
+    cin>>num;
+    for(int i=0;i<num;i++){
+        cin>>a[i];
+    }
+    for(int i=0;i<num;i++){
+        cin>>b[i];
+    }
+    sort(a,a+num);
+    sort(b,b+num);
+    for(int i=0;i<num;i++){
+        sum+=(a[i]*b[num-1-i]);
+    }
+    cout<<sum;
+    return 0;
+}
+/*
+3
+1 3 -5
+-2 4 1
+*/
